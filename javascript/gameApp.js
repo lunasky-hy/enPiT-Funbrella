e2786720c0a928f17909de3572ceb32445950acd
wakkihaya <wakkihaya@gmail.com>
2019-08-05 13:36:37 +0900

    敵との当たり判定を実装

--- javascript/gameApp.js
+++ javascript/gameApp.js
@@ -6,16 +6,25 @@ phina.globalize();
 
 var ASSETS = {
   image:{
-    'umbrella': 'https://kohacu.com/wp-content/uploads/2018/05/kohacu.com_000102_20170830-300x300.png',
+        //'umbrella': 'https://www.illust-box.jp/db_img/sozai/00010/108866/watermark.jpg',
+
+     'umbrella': 'https://kohacu.com/wp-content/uploads/2018/05/kohacu.com_000102_20170830-300x300.png',
       'thunder':'https://www.sozailab.jp/db_img/sozai/13085/9e5baae2f2a6c96a62655fc3bdd8d10c.png'
   },
 };
 
 // the size of kaminari
 var THUNDER_WIDTH = 100;
-var THUNDER_HEIGHT = 155;
+var THUNDER_HEIGHT = 110;
 var SCREEN_WIDTH = 500;
 var SCREEN_HEIGHT = 800;
+var UMBRELLA_WIDTH = 70;
+var UMBRELLA_HEIGHT = 80;
+
+//円判定の円
+var HIT_RADIUS = 30;
+//傘のライフ
+var life = 1;
 
 
 
@@ -31,15 +40,16 @@ phina.define('MainScene', {
     var umbrella = Sprite('umbrella').addChildTo(this);
     umbrella.x = this.gridX.center();
     umbrella.y =800;
-    umbrella.scaleX = 0.3;
-    umbrella.scaleY = 0.3;
-    
+
+      umbrella.height = UMBRELLA_HEIGHT;
+      umbrella.width = UMBRELLA_WIDTH;
+      umbrella.padding =0;
     this.player = umbrella;
     
     //thunder  group の生成
       this.thunderGroup = phina.display.CanvasElement().addChildTo(this);
       
-      //thuner生成notameno time
+      //thuner生成のための軸を生成
       this.timer = 0;
     
 
@@ -59,16 +69,44 @@ phina.define('MainScene', {
 
     ++this.timer;
     
-    //thunderの生成
+    //thunderの生成,  30秒ごとにobjectを生成
         if(this.timer %30 === 0) {
             var thunder = Thunder().addChildTo(this.thunderGroup);
             thunder.x = Math.randint(0, SCREEN_WIDTH);
             thunder.y = 0 - SCREEN_HEIGHT;
         }
-  }
+
+        //当たり判定をする
+        var self = this;
+        self.hitTestEnemyPlayer();
+
+  },
+    //ディスプレイ傘と雷の当たり判定を円でするメソッド
+    hitTestEnemyPlayer : function(){
+      var player = this.player;
+      var self = this;
+
+      this.thunderGroup.children.each(function(thunder){
+          var c1 = Circle(player.x, player.y, HIT_RADIUS);
+          var c2 = Circle(thunder.x, thunder.y , HIT_RADIUS);
+            
+          // 当たってるか判定
+          if(Collision.testCircleCircle(c1,c2)){
+              life--;
+              
+              //lifeが0になったら、終了
+              if(life === 0) {
+                  console.log("hit");
+              }
+          }
+
+      })
+
+    }
+
 });
 
-//thunderのオブジェクトclass seisei
+//thunderのオブジェクトclass 生成
 phina.define("Thunder",{
     superClass: 'phina.display.Sprite',
     
